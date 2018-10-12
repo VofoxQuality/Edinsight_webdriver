ef589c94ad8dd423c7c776bffc192ee003047d26
VOFOX\sebastian <sebansoney@gmail.com>
2018-10-12 18:36:02 +0530

    12-10-2018 18:35

--- src/main/java/Library/BaseClassOne.java
+++ src/main/java/Library/BaseClassOne.java
@@ -54,9 +54,9 @@ public class BaseClassOne	{
 	 public static WebDriver driver;
 	 public String  baseUrl = "http://192.168.10.124/Edinsight/Login.aspx";
 	 //Sebastian
-	// protected  String downloadPath="D:\\selenium_downloads";
+	 protected  String downloadPath="D:\\selenium_downloads";
 	 //Manoj
-	protected  String downloadPath="E:\\selenium_downloads";
+	//protected  String downloadPath="E:\\selenium_downloads";
 	 
 	 int i=0;
 	
--- src/main/java/com/curriculum/Batch_Print.java
+++ src/main/java/com/curriculum/Batch_Print.java
@@ -139,7 +139,7 @@ public class Batch_Print extends BaseClassOne
 			click("//*[@id='ctl00_MainContent_linkPdf']");			
 			
 			//Assert the message "You must select at least one teacher."
-			Assert.assertEquals(getText("//*[@id='ctl00_MainContent_lblMsg']"), "You must select at least one teacher." ,"Message-You must select at least one teacher-not found");
+			Assert.assertEquals("You must select at least one teacher.",getText("//*[@id='ctl00_MainContent_lblMsg']"),"Message-You must select at least one teacher-not found");
 			
 			js.executeScript("scroll(0,document.body.scrollHeight)");
 			
@@ -150,7 +150,7 @@ public class Batch_Print extends BaseClassOne
 			click("//*[@id='ctl00_MainContent_linkWord']");		
 			
 			//Assert the message "You must select at least one teacher."			
-			Assert.assertEquals(getText("//*[@id='ctl00_MainContent_lblMsg']"), "You must select at least one teacher." ,"Message-You must select at least one teacher-not found");
+			Assert.assertEquals("You must select at least one teacher.",getText("//*[@id='ctl00_MainContent_lblMsg']"),"Message-You must select at least one teacher-not found");
 
 		} 
 		
