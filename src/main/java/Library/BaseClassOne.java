929d9ff89d1c632e48e3f3fa8d081c6073844e41
VOFOX\Akhil-TS <akhilts@vofoxsolutions.con>
2018-11-09 17:15:30 +0530

    akhil-11-9-18

--- src/main/java/Library/BaseClassOne.java
+++ src/main/java/Library/BaseClassOne.java
@@ -79,12 +79,12 @@ public class BaseClassOne	{
 		//Sebastian
 		//System.setProperty("webdriver.gecko.driver","D:\\Jars\\Drivers\\New-geckodriver\\geckodriver.exe");
 		//Akhil TS
-		//System.setProperty("webdriver.gecko.driver", "E:\\Edinsight\\geckodriver.exe");
+		System.setProperty("webdriver.gecko.driver", "E:\\Edinsight\\geckodriver.exe");
 		//Manoj
 
 		//System.setProperty("webdriver.gecko.driver", "f:\\Jars\\geckodriver.exe");
 		//Server
-		System.setProperty("webdriver.gecko.driver", "e:\\Jars\\geckodriver.exe");
+		//System.setProperty("webdriver.gecko.driver", "e:\\Jars\\geckodriver.exe");
 
 		//System.setProperty("webdriver.gecko.driver", "f:\\Jars\\geckodriver.exe");
 
