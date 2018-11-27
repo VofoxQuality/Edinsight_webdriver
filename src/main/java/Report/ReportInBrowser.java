7b7e8df8783fde793dc531e9d322003919313f49
VOFOX\sebastian <sebansoney@gmail.com>
2018-11-27 11:26:01 +0530

    conflict 11:25

--- src/main/java/Report/ReportInBrowser.java
+++ src/main/java/Report/ReportInBrowser.java
@@ -2,8 +2,11 @@ package Report;
 
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
 
 public class ReportInBrowser {
 	
@@ -20,6 +23,9 @@ public class ReportInBrowser {
 		driver1.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 			//driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 		driver1.get("file:///e:/Quality/trunk/Edinsight_Maven_Automation/target/test-output/emailable-report.html");
+		
+		Actions action = new Actions(driver); 
+		action.sendKeys(Keys.RIGHT).build().perform();`
 	}
 
 }
